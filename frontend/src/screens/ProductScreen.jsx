cf1c4a7ddb4006a2cd68064d4d008afc172e16ba
Luke Cheng <Lukeecheng@gmail.com>
2024-12-02 23:03:03 -0800

    completed Qty UI for addToCart. Tested to work with Redux dev tool and local storage

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
+import {Row, Col, Image, ListGroup, Card, Button, Form} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader'
 import Message from '../components/Message';
-
+import { useState } from 'react';
+import { addToCart } from '../slices/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const ProductScreen = () => {
   // renaming id to productId
   const { id: productId } = useParams();
 
+  // use dispatch to call addToCart
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  // set product qty
+  const [qty, setQty] = useState(1);
+
   // individual productApiSlice replacing fetch() with redux
   const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({...product, qty}));
+    navigate('/cart');
+  }
 
   return (
     <>
@@ -69,9 +83,28 @@ const ProductScreen = () => {
               </Row>
             </ListGroup.Item>
 
+            {/* show this if stock > 0 */}
+            {product.countInStock > 0 && (
+              <ListGroup.Item>
+                <Row>
+                  <Col>Qty</Col>
+                  <Col>
+                    <Form.Control as='select' value={qty} onChange={(e) => setQty(Number(e.target.value)) }>
+                      {/* number of qty for user to select is based on stock */}
+                      {/* below uses an array to get the number of product in stock, then how 'keys' to add 1 to each index to prevent 0 */}
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          { x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            )}
             <ListGroup.Item>
               {/* if out of stock, button will no be clickable */}
-              <Button className='btn-block' type='button' disabled={product.countInStock === 0} >
+              <Button className='btn-block' type='button' disabled={product.countInStock === 0} onClick={addToCartHandler}>
                 Add To Cart
               </Button>
             </ListGroup.Item>
