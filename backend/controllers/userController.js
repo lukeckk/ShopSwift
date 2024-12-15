b4e190c8a7a002896eeda0dffbcfd06cf68a25b5
Luke Cheng <Lukeecheng@gmail.com>
2024-12-14 18:01:51 -0800

    configured logoutUser function to clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST / api/users/logout
 // @access Private 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('logout user');
+  // set cookie with naem 'jwt' to expire
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+
+  res.status(200).json({ message: 'Logged out successfully' }); 
 });
 
 // @desc Get user profile
